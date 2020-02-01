3d1b54a295fc5fbc145a8368428fc54407c2a302
ephe-meral <j.appel.online@gmail.com>
2020-02-01 10:52:35 +0100

    init app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Onsen specific styling
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
