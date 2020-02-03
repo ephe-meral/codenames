9be6db639fd36a6f77c24d54094abb35c809c6e3
ephe-meral <j.appel.online@gmail.com>
2020-02-03 20:30:29 +0100

    update codenames rendering code

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### codenames
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3c689ead-a11b-49bc-a3df-1ee8e390c78f/deploy-status)](https://app.netlify.com/sites/codenames-pwa/deploys)
+
 A small test app (PWA) implementing a codenames-like game steered by AI (Word2Vec).
 Currently [WIP], but should compile and run regardless.
 
--- src/components/CodeNames.js
+++ src/components/CodeNames.js
@@ -1,34 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/macro';
-import { Card, Col, Row } from 'react-onsenui';
+import { Button, Card, Col, Row } from 'react-onsenui';
+import * as data from '../data/codenames187.json';
 
-const CodeNames = () => (
+const NUM_CARDS = 12;
+
+const addRndWord = selected => {
+  if (selected.length >= data.words.length) {
+    return selected;
+  }
+
+  const newWord = data.words[Math.floor(Math.random() * data.words.length)];
+
+  if (selected.find(s => s === newWord)) {
+    return addRndWord(selected);
+  }
+  return [newWord, ...selected];
+};
+
+const selectRndCards = count =>
+  Array(count)
+    .fill(0)
+    .reduce(acc => addRndWord(acc), []);
+
+const selectRndColors = count =>
+  Array(count)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * 2));
+
+const ColCard = ({ colored, disabled, onClick, word }) => (
   <Col>
-    <Row>
-      <Col>
-        <Card>
-          <p>test</p>
-        </Card>
-      </Col>
-      <Col>
-        <Card>
-          <p>test</p>
-        </Card>
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <Card>
-          <p>test</p>
-        </Card>
-      </Col>
-      <Col>
-        <Card>
-          <p>test</p>
-        </Card>
-      </Col>
-    </Row>
+    <div css="margin: 0.25em">
+      <Button
+        css={`
+          width: 100%;
+          ${colored ? 'background-color: red' : ''}
+        `}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        <p css="text-align: center;">{word}</p>
+      </Button>
+    </div>
   </Col>
 );
+
+const CodeNames = () => {
+  const [board, setBoard] = useState(selectRndCards(NUM_CARDS));
+  const [colors, setColors] = useState(selectRndColors(NUM_CARDS));
+  const [selected, setSelected] = useState(Array(NUM_CARDS).fill(0));
+
+  if (NUM_CARDS % 2 && NUM_CARDS % 3) {
+    return null;
+  }
+
+  const cols = !(NUM_CARDS % 3) ? 3 : 2;
+  const rows = NUM_CARDS / cols;
+
+  console.log(board);
+  console.log(colors);
+  console.log(selected);
+
+  return (
+    <Col>
+      {Array(rows)
+        .fill(null)
+        .map((r, i) => (
+          <Row key={`${i}`}>
+            {Array(cols)
+              .fill(null)
+              .map((c, j) => (
+                <ColCard
+                  key={`${i},${j}`}
+                  colored={!!selected[cols * i + j] && !!colors[cols * i + j]}
+                  disabled={!!selected[cols * i + j]}
+                  word={board[cols * i + j]}
+                  onClick={() => setSelected(Object.assign([...selected], { [cols * i + j]: 1 }))}
+                />
+              ))}
+          </Row>
+        ))}
+    </Col>
+  );
+};
 
 export { CodeNames };
